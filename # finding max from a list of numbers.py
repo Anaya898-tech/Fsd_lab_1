27cf1ea089b7803170f54f753aa6953f2782fbe2
Anaya Shah <115178357+Anaya898@users.noreply.github.com>
2023-08-03 19:56:20 +0530

    Add files via upload

---
+++ # finding max from a list of numbers.py
@@ -0,0 +1,7 @@
+# finding max from a list of numbers 
+a=[int (i) for i in input("Enter a list of numbers: ").split()]
+max_list=max(a)
+print("Maximum of the numbers is: ",max_list)
+min_list=min(a)
+print("Minimum of the numbers is: ",min_list)
+   
