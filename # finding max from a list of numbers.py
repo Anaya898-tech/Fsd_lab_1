b2a7b41e4e80bcf48dfa6f8d57490ed66853dafc
Anaya Shah <115178357+Anaya898@users.noreply.github.com>
2023-08-04 12:39:54 +0530

    Update # finding max [without max funtion] from a list of numbers.py

--- # finding max from a list of numbers.py
+++ # finding max from a list of numbers.py
@@ -1,7 +1,18 @@
-# finding max from a list of numbers 
-a=[int (i) for i in input("Enter a list of numbers: ").split()]
-max_list=max(a)
-print("Maximum of the numbers is: ",max_list)
-min_list=min(a)
-print("Minimum of the numbers is: ",min_list)
-   
+def find_max(lst):
+    if not lst:
+        return None
+
+    max_element = lst[0]
+    for element in lst:
+        if element > max_element:
+            max_element = element
+
+    return max_element
+
+# Take user input for the list of numbers
+try:
+    input_list = [int(num) for num in input("Enter a list of numbers separated by spaces: ").split()]
+    print("List:", input_list)
+    print("Max element:", find_max(input_list))
+except ValueError:
+    print("Invalid input! Please enter a list of numbers separated by spaces.")
